556bf17b16d0631983b8fc5e1d0c00b5f35937e2
Gabriellungu123 <gabilungu913@gmail.com>
2025-12-23 09:57:16 +0100

    Pasito 2

--- main.py
+++ main.py
@@ -1,11 +1,24 @@
 from sanic import Sanic
 from sanic.response import text
+from sanic_ext import Extend, render
 
 app = Sanic("MyHelloWorldApp")
 
+# ACTIVAR SANIC-EXT
+Extend(app)
+
+# Servir estáticos
+app.static("/static", "./static")
+
 @app.get("/")
-async def hello_world(request):
-    return text("Hello, world.")
+async def index(request):
+    return await render("login.html")
+
+@app.post("/login")
+async def login(request):
+    username = request.form.get("username")
+    password = request.form.get("password")
+    return text(f"Usuario: {username}, Contraseña: {password}")
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8000, debug=True)
